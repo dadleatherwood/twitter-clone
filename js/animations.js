6f4432d4bf6e885445f79ab5a85f3801bb84d848
Jacob Leatherwood <jacob.leatherwood.22@gmail.com>
2017-05-25 16:29:48 -0500

    getting started

---
+++ js/animations.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  //Hide Tweet Button
+  $('#tweet-controls').hide();
+
+  //Click on text-area double text size, reveal tweet button,  and character count
+  $
+
+
+});
