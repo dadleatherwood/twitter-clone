55f02ffda16cdb07351f24032f459835934396f2
Jacob Leatherwood <jacob.leatherwood.22@gmail.com>
2017-05-25 16:53:16 -0500

    working on tweet box

--- js/animations.js
+++ js/animations.js
@@ -3,7 +3,10 @@ $(document).ready(function(){
   $('#tweet-controls').hide();
 
   //Click on text-area double text size, reveal tweet button,  and character count
-  $
+  $('.tweet-compose').on('click', function(){
+    $('#dashboard').css('height') //double
+    $('#tweet-controls').show(); //reveal button and character count
+  })
 
 
 });
