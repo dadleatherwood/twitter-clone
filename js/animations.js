aa6ff94cb895cdd60b80f38b69bd73ab85565bf1
Jacob Leatherwood <jacob.leatherwood.22@gmail.com>
2017-05-26 14:12:18 -0500

    completed tweet box portion

--- js/animations.js
+++ js/animations.js
@@ -1,12 +1,29 @@
 $(document).ready(function(){
   //Hide Tweet Button
+  var $charCount = $('#char-count')
+  var characterCount = 140;
   $('#tweet-controls').hide();
 
   //Click on text-area double text size, reveal tweet button,  and character count
   $('.tweet-compose').on('click', function(){
-    $('#dashboard').css('height') //double
+    $('.tweet-compose').css('height','5em'); //double
     $('#tweet-controls').show(); //reveal button and character count
-  })
+  });
+
+  //character count should decrease when a person types, and then change red when it reaches 10 or below
+  $('.tweet-compose').keyup(function(){
+    var length = $(this).val().length
+    // make the 140 something that can change?
+    characterCount = 140 - length
+    $charCount.text(characterCount)
+
+    if(characterCount <= 10){
+      $charCount.css('color','red');
+    } else if (characterCount > 10){
+      $charCount.css('color', 'black');
+    }
+  });
+
 
 
 });
