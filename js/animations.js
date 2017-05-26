2498a515192f2f9b9fdbd523b94d31b0d02d0118
Jacob Leatherwood <jacob.leatherwood.22@gmail.com>
2017-05-26 15:27:51 -0500

    successful tweet

--- js/animations.js
+++ js/animations.js
@@ -1,19 +1,24 @@
 $(document).ready(function(){
-  //Hide Tweet Button
+  //important variables
   var $charCount = $('#char-count')
   var characterCount = 140;
+  var $tweetCompose = $('.tweet-compose')
+
+
+  //Hide Tweet Button
   $('#tweet-controls').hide();
 
   //Click on text-area double text size, reveal tweet button,  and character count
-  $('.tweet-compose').on('click', function(){
-    $('.tweet-compose').css('height','5em'); //double
+  $tweetCompose.on('click', function(){
+    $tweetCompose.css('height','5em'); //double
     $('#tweet-controls').show(); //reveal button and character count
   });
 
   //character count should decrease when a person types, and then change red when it reaches 10 or below
-  $('.tweet-compose').keyup(function(){
+  $tweetCompose.keyup(function(){
     var length = $(this).val().length
-    // make the 140 something that can change?
+
+    // make the 140 something that can be changed
     characterCount = 140 - length
     $charCount.text(characterCount)
 
@@ -22,7 +27,36 @@ $(document).ready(function(){
     } else if (characterCount > 10){
       $charCount.css('color', 'black');
     }
+
+  //disable tweet button if the user excedes 140 characters
+    if(characterCount <= 0){
+      $('.button').attr('disabled', 'disabled');
+    }
+
+    if (characterCount > 0){
+       $('.button').removeAttr('disabled','disabled');
+       };
+
+
   });
+
+  //successfully post a tweet (NEW FUNCTION)
+  $('.button').on('click',function(){
+      var tweet = $tweetCompose.val()
+    $('.tweet').prepend(
+      '<img class="avatar" src="img/alagoon.jpg" />' +
+      '<strong class="fullname">Jacob Leatherwood </strong>' +
+      '<span class="username">@iRockYouDont</span>' +
+      '<p class="tweet-text">' + tweet + '</p>'
+    );
+
+  });
+
+
+
+
+
+
 
 
 
